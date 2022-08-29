b95e544b2188be7a492722868eea3d84c3585571
Mykhail <dsrdsr7@gmail.com>
2022-08-29 20:28:52 +0300

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {compose, legacy_createStore as createStore} from "redux";
 import {rootReducer} from "./redux/rootReducer";
 import {Provider} from "react-redux"
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import AppWrapper from "./App_wrapper";
 
 const store = createStore(rootReducer, compose(
@@ -16,11 +15,11 @@ const store = createStore(rootReducer, compose(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <Provider store={store}>
             <AppWrapper />
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
 
